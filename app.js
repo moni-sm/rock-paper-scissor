e25bd7ac4b671f8bd6e0573e7efaeff5f3b663f2
Monika SM <monikasm2019@gmail.com>
2025-01-29 02:57:09 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,69 @@
+let userScore = 0;
+let compScore = 0;
+
+
+const choices = document.querySelectorAll(".choice");
+const msg = document.querySelector("#msg");
+
+const userScorePara = document.querySelector("#user-score");
+const compScorePara = document.querySelector("#comp-score");
+
+const genCompChoice = () =>{
+    const options =["rock","paper","scissors"];
+    const randIdx = Math.floor(Math.random()*3);
+    return options[randIdx];
+}
+
+const drawGame = () =>{
+    msg.innerText="Game is Draw play again";
+    msg.style.backgroundColor="aqua";
+}
+
+const showWinner =(userwin,userChoice,compChoice)=>{
+    if(userwin){
+        userScore++;
+        userScorePara.innerText = userScore;
+        msg.innerText=`You Win! ${userChoice} beats y ${compChoice}`;
+        msg.style.backgroundColor="green";
+    }
+    else{
+        compScore++;
+        compScorePara.innerText = compScore;
+        msg.innerText=`You Loose  ${compChoice} beats your ${userChoice}`;
+        msg.style.backgroundColor="red";
+    }
+}
+
+const playGame =(userChoice)=>{
+    //Generate computer choice
+    const compChoice = genCompChoice();
+    console.log(compChoice);
+
+
+    if(userChoice === compChoice){
+        drawGame();
+    }else{
+        let userwin = true;
+        if(userChoice === "rock"){
+            //scissors papers
+            userwin = compChoice === "paper" ? false : true;
+        }
+        else if(userChoice === "paper"){
+            // rock scissor
+           userwin = compChoice === "scissors" ? false : true;
+        }
+        else{
+            //rock paper
+            userwin = compChoice === "rock"? false : true;
+        }
+        showWinner(userwin,userChoice,compChoice);
+    }
+}
+
+choices.forEach((choice)=>{
+    choice.addEventListener("click", ()=>{
+        const userChoice = choice.getAttribute("id");
+        playGame(userChoice);
+
+    })
+})
